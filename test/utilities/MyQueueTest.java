665cddd06b5070255f3425842fa077e4be1fb318
Yassinenali01 <118232464+Yassinenali01@users.noreply.github.com>
2023-11-24 11:42:29 -0700

    Update MyQueueTest.java
    MyQueueTest is done

--- test/utilities/MyQueueTest.java
+++ test/utilities/MyQueueTest.java
@@ -121,6 +121,36 @@ public class MyQueueTest {
         assertEquals("123", result.toString());
     }
 	
+	// still to modified; couldnt implenent the total logic.
+	@Test
+	void testIsFull() {
+		
+		queue = new MyQueue<>();
+	    queue.enqueue(1);
+	    queue.enqueue(2);
+	    queue.enqueue(3);
+	    
+	    
+	    assertEquals(3, queue.size());
+		
+	}
+	
+	@Test
+	void testSize() {
+		
+		queue = new MyQueue<>();
+
+		
+        assertEquals(0, queue.size());
+
+	    queue.enqueue(1);
+	    queue.enqueue(2);
+	    assertEquals(2, queue.size());
+	
+	    queue.dequeue();
+        assertEquals(1, queue.size());
+	    
+	}
 	
 	
 	
